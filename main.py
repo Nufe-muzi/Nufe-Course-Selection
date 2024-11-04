617f5c571ad58b11305beb71fc61625b75c96de2
Lalo <91353515+Nufe-muzi@users.noreply.github.com>
2024-11-04 21:12:34 +0800

    更改多选课程逻辑
    增加多门选课机制

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from datetime import datetime
 import requests
 from auto_cookie import get_cookie
 import winsound
-
+import sys
 studentAssoc = 0 #学生ID
 courseSelectTurnAssoc = 0 #学期ID
 lessonAssoc = 0 #课程ID
@@ -44,7 +44,7 @@ data_request = {
     "coursePackAssoc": None
 }
 
-#协议请求头 只用寻找 Cookie
+#协议请求头 只用寻找 Authorization
 headers = {
     # 'Accept': 'application/json, text/plain, */*',
     # 'Accept-Encoding': 'gzip, deflate, br',
@@ -52,7 +52,7 @@ headers = {
     'Authorization': '',
     # 'Content-Length': '134',
     # 'Content-Type': 'application/json',
-    # 'Cookie': '',
+    # 'Cookie': 'cs-course-select-student-token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJzdXB3aXNkb20iLCJleHAiOjE3MjU5MDAyMDgsInVzZXJuYW1lIjoiMjEyMDIzMTM0MiJ9.lYL-TsLjDi9nkDfoE0_v4ij503j9XKDk_pMs5urfZK0',
     # 'Origin': 'https://jwxt.nufe.edu.cn',
     # 'Referer': 'https://jwxt.nufe.edu.cn/course-selection/',
     # 'Sec-Ch-Ua': '"Not)A;Brand";v="24", "Chromium";v="116"',
@@ -161,6 +161,7 @@ def main():
                             
 
                             print(f"选课成功,课程ID：{lessonAssoc},课程名称:{target_name}")
+                            time.sleep(100000)
                             return True
                 except requests.exceptions.ConnectionError as e:
                     print(f"Connection error: {e}. [error id]=1")
@@ -190,11 +191,11 @@ class Login_Data:
 
 
 def DataInput():
-    username = input("请输入您的账号: ")
-    password = input("请输入您的密码: ")
-    target_teacher_name = input("请输入您的选课老师名字: ")
+    username = input("请输入您的账号(输完回车): ")
+    password = input("请输入您的密码(输完回车): ")
+    target_teacher_name = input("请输入您的选课老师名字(没有就回车): ")
     target_name = input("请输入您的课程名称(带上课程数字编号 如 网球3 ): ): ")
-    target_courseCode = input("请输入您的课程代码(可空): ")
+    target_courseCode = input("请输入您的课程代码(没有就回车): ")
     
     month = int(input("请输入抢课月份(1-12): "))
     day = int(input("请输入抢课开始日(1-31): "))
@@ -206,11 +207,38 @@ def DataInput():
     return Login_Data(username,password,target_teacher_name,target_name,target_courseCode,predict_Time)
 
 if __name__ == "__main__":
-
+    print("软工班特供版,请在抢课提前2小时前打开此程序(能显示预览界面的时候)。")
+    # 这是加入白名单
+    whitelist=[2120243448,2120242335,2120241946,2120242852,2120241188,2120243152
+               ,2120243019,2120240981,2120243525,2120242760,2120233697,2120221309,
+                2120221109, 2120223024, 2120230029, 2120231160, 2120231281,
+                2120231369, 2120231382, 2120231383, 2120231442, 2120231481,
+                2120231493, 2120231571, 2120231641, 2120231671, 2120231716,
+                2120231718, 2120231749, 2120231777, 2120231869, 2120231876,
+                2120231913, 2120231933, 2120232018, 2120232067, 2120232213,
+                2120232272, 2120232276, 2120232292, 2120232311, 2120232330,
+                2120232421, 2120232448, 2120232457, 2120232465, 2120232477,
+                2120232559, 2120232597, 2120232636, 2120232704, 2120232737,
+                2120232787, 2120232961, 2120232969, 2120233007, 2120233009,
+                2120233069, 2120233130, 2120233151, 2120233159, 2120233196,
+                2120233206, 2120233212, 2120233233, 2120233312, 2120233316,
+                2120233336, 2120233357, 2120233372, 2120233392, 2120233422,
+                2120233524, 2120233536, 2120233679, 2120233697, 2120233707,
+                2120233736, 2120233770, 2120233776, 2120233983, 2120233984,
+                2120234161, 2120234174, 4120230028, 4120230030
+               ]
+    
     list = DataInput()
 # ===================================================
 # 下列是让用户在窗口输入账号登录
     username = list.username
+    if int(username) not in whitelist:
+        print("不在白名单中，启动自毁程序，你的电脑即将自毁。倒计时")
+        for i in range(10, 0, -1):
+            print(i)
+            time.sleep(1)
+        time.sleep(10)
+        sys.exit()
     password = list.password
     target_teacher_name = list.target_teacher_name
     target_name = list.target_name
@@ -225,31 +253,70 @@ if __name__ == "__main__":
     # password = ""  # 密码
 # ===================================================
 
-    predict_Time = datetime(2024, 9, 12, 12, 49, 0) # 预测抢课时间
+    # predict_Time = datetime(2024, 11, 4, 20, 16, 0) # 预测抢课时间
 
 
 
     headers['Authorization'] = get_cookie(username,password)
     stuID = requests.get(url_get_stuID, headers=headers)
+    print(stuID)
     stuID_json = stuID.json()
     if stuID_json.get("data") == []:
-        print("获取学生ID失败，请检查是否已经开放选课预览页面")
-        exit()
+        print("获取学生ID失败，请检查是否已经开放选课预览页面。程序退出")
+        time.sleep(10)
+        sys.exit()
     studentAssoc = stuID_json.get("data")[0]
 
+
+    # courseSelectTurnAssoc = requests.get(url_OpenTurns + str(studentAssoc), headers=headers)
+    # courseSelectTurnAssoc_json = courseSelectTurnAssoc.json()
+    # courseSelectTurnAssoc = courseSelectTurnAssoc_json.get("data")[0].get("id")
+
+    # select = requests.get(url_select + str(studentAssoc) +'/turn/' + str(courseSelectTurnAssoc) + '/select'  , headers=headers)
+    # select_json = select.json()
+    # semesterId = select_json.get("data").get('turn').get("semesterAssoc")
+    # campusId = select_json.get("data").get("campusId")
+    
+
+    # 遍历所有元素并获取 id
     courseSelectTurnAssoc = requests.get(url_OpenTurns + str(studentAssoc), headers=headers)
     courseSelectTurnAssoc_json = courseSelectTurnAssoc.json()
-    courseSelectTurnAssoc = courseSelectTurnAssoc_json.get("data")[0].get("id")
+    course_ids = [item.get("id") for item in courseSelectTurnAssoc_json.get("data")]
+    course_name = [item.get("name") for item in courseSelectTurnAssoc_json.get("data")]
+    i = 0
+    results = []
 
-    select = requests.get(url_select + str(studentAssoc) +'/turn/' + str(courseSelectTurnAssoc) + '/select'  , headers=headers)
-    select_json = select.json()
-    semesterId = select_json.get("data").get('turn').get("semesterAssoc")
-    campusId = select_json.get("data").get("campusId")
-    
+    for course_id in course_ids:
+        select = requests.get(url_select + str(studentAssoc) + '/turn/' + str(course_id) + '/select', headers=headers)
+        
+        select_json = select.json()
+        semesterId = select_json.get("data").get('turn').get("semesterAssoc")
+        campusId = select_json.get("data").get("campusId")
+        # 将结果存入数组
+        results.append({
+            "turn_id": course_id,
+            "semester_id": semesterId,
+            "campus_id": campusId,
+            "course_name": course_name[i]
+        })
+
+        i = i+1
+        
+    for i, result in enumerate(results):
+        print(f"选择序号: {i}, 课程名称: {result['course_name']}, Course ID: {result['turn_id']}, Semester ID: {result['semester_id']}, Campus ID: {result['campus_id']}")
+
+    index = int(input("请输入要选择的序号:"))
+
+    selected_course = results[index]
+    selected_campus_id = selected_course["campus_id"]
+    selected_semesterId = selected_course["semester_id"]
+    selected_turn_id = selected_course["turn_id"]
+    selected_course_name = selected_course["course_name"]
+
 
     data_course = {
     "adminclassId": "",
-    "campusId": campusId,
+    "campusId": selected_campus_id,
     "canSelect": 1,
     "courseNameOrCode": target_courseCode,
     "coursePropertyId": "",
@@ -266,18 +333,18 @@ if __name__ == "__main__":
     "openDepartmentId": "",
     "pageNo": 1,
     "pageSize": 20,
-    "semesterId": semesterId,
+    "semesterId": selected_semesterId,
     "sortField": "course",
     "sortType": "ASC",
     "studentId": studentAssoc,
     "substitutedCourseId": None,
     "teacherNameOrCode": target_teacher_name,
-    "turnId": courseSelectTurnAssoc,
+    "turnId": selected_turn_id,
     "week": "",
     "_canSelect": "可选"
 }
 
-    course = requests.post(url_Lesson + str(studentAssoc) +'/' + str(courseSelectTurnAssoc), headers=headers, json=data_course)
+    course = requests.post(url_Lesson + str(studentAssoc) +'/' + str(selected_turn_id), headers=headers, json=data_course)
     course_josn = course.json()
     for lesson in course_josn['data']['lessons']:
         if lesson['course']['nameZh'] == target_name:
@@ -290,7 +357,7 @@ if __name__ == "__main__":
     # lesson_names = [lesson['minorCourse']['nameZh'] for lesson in lessons]
     data = {
         "studentAssoc": studentAssoc,
-        "courseSelectTurnAssoc": courseSelectTurnAssoc,
+        "courseSelectTurnAssoc": selected_turn_id,
         "requestMiddleDtos": [
             {
                 "lessonAssoc": lessonAssoc,
@@ -301,7 +368,7 @@ if __name__ == "__main__":
     }
     data_request = {
         "studentAssoc": studentAssoc,
-        "courseSelectTurnAssoc": courseSelectTurnAssoc,
+        "courseSelectTurnAssoc": selected_turn_id,
         "requestMiddleDtos": [
             {
                 "lessonAssoc": lessonAssoc,
@@ -312,8 +379,9 @@ if __name__ == "__main__":
     }
     print('请确认即将抢课信息：')
     print(f'学生信息:ID:{studentAssoc}')
-    print(f"课程名称：{lesson_names}, 课程ID：{lessonAssoc}，授课教师：{target_teacher_name}" )
-    time.sleep(1)
+    print(f"选课大类名称：{selected_course_name}, 获取到的课程ID：{lessonAssoc}，授课教师：{target_teacher_name}" )
+    time.sleep(2)
     url_predicate_response_orgin = url_predicate_response_orgin + str(studentAssoc) +'/'
     url_add_drop_response_orgin = url_add_drop_response_orgin + str(studentAssoc) +'/'
     main()
+    time.sleep(100000)
